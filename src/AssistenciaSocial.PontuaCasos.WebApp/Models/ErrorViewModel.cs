b96bb02aeac1629d03694a7c2efad8b6868fb86e
Roberto de Oliveira Santos <robertoosan@gmail.com>
2022-08-12 02:01:01 +0000

    Versão Inicial

---
+++ src/AssistenciaSocial.PontuaCasos.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AssistenciaSocial.PontuaCasos.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
