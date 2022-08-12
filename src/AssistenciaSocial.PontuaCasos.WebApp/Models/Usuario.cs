15ab02cd6fdc1a81831854aa5610c36119f7272d
Roberto de Oliveira Santos <robertoosan@gmail.com>
2022-08-12 21:28:26 +0000

    Tratamento de nulos

--- src/AssistenciaSocial.PontuaCasos.WebApp/Models/Usuario.cs
+++ src/AssistenciaSocial.PontuaCasos.WebApp/Models/Usuario.cs
@@ -3,9 +3,9 @@ public class Usuario
 {
     public int Id { get; set; }
     public string Nome { get; set; } = null!;
-    public List<Organizacao> Organizacoes { get; set; } = null!;
+    public List<Organizacao>? Organizacoes { get; set; }
     public DateTime CriadoEm { get; set; }
-    public Usuario CriadoPor { get; set; } = null!;
+    public Usuario? CriadoPor { get; set; }
     public DateTime ModificadoEm { get; set; }
-    public Usuario ModificadoPor { get; set; } = null!;
+    public Usuario? ModificadoPor { get; set; }
 }
