f94e8a2e34f8088d952a71a9875041d55273703f
Roberto de Oliveira Santos <robertoosan@gmail.com>
2022-08-17 21:18:38 +0000

    Criar Itens das Categorias

---
+++ src/AssistenciaSocial.PontuaCasos.WebApp/Controllers/ItensController.cs
@@ -0,0 +1,173 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AssistenciaSocial.PontuaCasos.WebApp.Models;
+
+namespace AssistenciaSocial.PontuaCasos.WebApp.Controllers
+{
+    public class ItensController : Controller
+    {
+        private readonly PontuaCasosContext _context;
+
+        public ItensController(PontuaCasosContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Itens
+        public async Task<IActionResult> Index()
+        {
+            var pontuaCasosContext = _context.Itens.Include(i => i.CriadoPor).Include(i => i.ModificadoPor);
+            return View(await pontuaCasosContext.ToListAsync());
+        }
+
+        // GET: Itens/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Itens == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Itens
+                .Include(i => i.CriadoPor)
+                .Include(i => i.ModificadoPor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // GET: Itens/Create
+        public IActionResult Create()
+        {
+            ViewData["CriadoPorId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["ModificadoPorId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Itens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(string? categoria_id, [Bind("Id,Titulo,Pontos,Ativo,Multiplo,Categoria,CriadoEm,CriadoPorId,ModificadoPorId,ModificadoEm")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CriadoPorId"] = new SelectList(_context.Users, "Id", "Id", item.CriadoPorId);
+            ViewData["ModificadoPorId"] = new SelectList(_context.Users, "Id", "Id", item.ModificadoPorId);
+            return View(item);
+        }
+
+        // GET: Itens/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Itens == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Itens.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            ViewData["CriadoPorId"] = new SelectList(_context.Users, "Id", "Id", item.CriadoPorId);
+            ViewData["ModificadoPorId"] = new SelectList(_context.Users, "Id", "Id", item.ModificadoPorId);
+            return View(item);
+        }
+
+        // POST: Itens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titulo,Pontos,Ativo,Multiplo,Categoria,CriadoEm,CriadoPorId,ModificadoPorId,ModificadoEm")] Item item)
+        {
+            if (id != item.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(item);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemExists(item.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CriadoPorId"] = new SelectList(_context.Users, "Id", "Id", item.CriadoPorId);
+            ViewData["ModificadoPorId"] = new SelectList(_context.Users, "Id", "Id", item.ModificadoPorId);
+            return View(item);
+        }
+
+        // GET: Itens/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Itens == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Itens
+                .Include(i => i.CriadoPor)
+                .Include(i => i.ModificadoPor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // POST: Itens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Itens == null)
+            {
+                return Problem("Entity set 'PontuaCasosContext.Itens'  is null.");
+            }
+            var item = await _context.Itens.FindAsync(id);
+            if (item != null)
+            {
+                _context.Itens.Remove(item);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemExists(int id)
+        {
+          return (_context.Itens?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
